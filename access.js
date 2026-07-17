69bf697bf1c604f0e72d61e78edff8f60be9d487
nandisha0 <nandisha0@users.noreply.github.com>
2026-07-17 13:07:47 +0000

    deploy: e157bd22950015414f95339c51dc127e543e3d8c

---
+++ access.js
@@ -0,0 +1,100 @@
+function getEventRoles(email, events, roles) {
+    const userRoles = roles.filter((r) => r.email === email);
+    const eventRoles = events.reduce((acc, e) => {
+        acc[e.id] = [];
+        return acc;
+    }, {});
+    eventRoles['*'] = [];
+    userRoles.forEach((r) => {
+        if (r.type === ROLES.OWNER) {
+            Object.keys(eventRoles).forEach((key) => eventRoles[key].push(r));
+        } else {
+            if (!eventRoles[r.event]) console.error('Event not found: ' + r.event);
+            eventRoles[r.event]?.push(r);
+        }
+    });
+    Object.keys(eventRoles).forEach((key) => {
+        if (eventRoles[key].length === 0) {
+            delete eventRoles[key];
+        }
+    });
+    return eventRoles;
+}
+
+function hasEventAccess(eventRoles, action, eventId = null) {
+    const isOwner = Boolean(eventRoles['*']);
+
+    if (!eventRoles[eventId]) {
+        if (action === ACTIONS.CREATE && isOwner) return true;
+        return false;
+    }
+
+    const isAdmin = isOwner || eventRoles[eventId].some((r) => r.type === ROLES.ADMIN);
+
+    if (action === ACTIONS.VIEW) return true;
+    if (action === ACTIONS.LOCK) return isAdmin;
+    return isOwner;
+}
+
+function hasRoleAccess(eventRoles, action, eventId, type = null) {
+    if (!eventRoles[eventId]) return false;
+
+    const isOwner = eventRoles[eventId].some((r) => r.type === ROLES.OWNER);
+    const isAdmin = isOwner || eventRoles[eventId].some((r) => r.type === ROLES.ADMIN);
+    const isEditor = isAdmin || eventRoles[eventId].some((r) => r.type === ROLES.EDITOR);
+
+    if (action === ACTIONS.VIEW) return isEditor;
+
+    if (type === ROLES.OWNER) return false;
+    if (type === ROLES.ADMIN) return isOwner;
+
+    return isAdmin;
+}
+
+function hasKeyAccess(eventRoles, action, eventId, language = null) {
+    if (!eventRoles[eventId]) return false;
+
+    const isOwner = eventRoles[eventId].some((r) => r.type === ROLES.OWNER);
+    const isAdmin = isOwner || eventRoles[eventId].some((r) => r.type === ROLES.ADMIN);
+    const isEditor =
+        isAdmin ||
+        eventRoles[eventId].some(
+            (r) =>
+                r.type === ROLES.EDITOR &&
+                (r.language === '*' || r.language === language || language === null),
+        );
+    const isViewer =
+        isEditor ||
+        eventRoles[eventId].some(
+            (r) => r.type === ROLES.VIEWER && (r.language === '*' || r.language === language),
+        );
+
+    if (action === ACTIONS.VIEW) return isViewer;
+    return isEditor;
+}
+
+const ACTIONS = {
+    VIEW: 'view',
+    CREATE: 'create',
+    UPDATE: 'update',
+    DELETE: 'delete',
+    LOCK: 'lock',
+};
+
+const ROLES = {
+    VIEWER: '0',
+    EDITOR: '1',
+    ADMIN: '2',
+    OWNER: '3',
+};
+
+const ROLE_MAP = {
+    [ROLES.VIEWER]: 'Viewer',
+    [ROLES.EDITOR]: 'Editor',
+    [ROLES.ADMIN]: 'Admin',
+    [ROLES.OWNER]: 'Owner',
+};
+
+const EVENT_STATUS = {
+    LOCKED: 'locked',
+};
